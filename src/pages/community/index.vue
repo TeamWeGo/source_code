8831848449ac6a14720576c36ba705ca0c228431
zxftuid <zxftuid@qq.com>
2019-04-21 15:33:07 +0800

    add api.md

--- src/pages/community/index.vue
+++ src/pages/community/index.vue
@@ -1,49 +1,127 @@
 <template>
   <div>
     <p class="ttt">{{ txt }}</p>
+    <button @click="insertOneUser">insertOneUser</button>
+    <button @click="queryOneUserByUserId">queryOneUserByUserId</button>
+    <button @click="updateOneUserById">updateOneUserById</button>
+    <button @click="insertOneTask">insertOneTask</button>
+    <button @click="queryOneTaskByTaskId">queryOneTaskByTaskId</button>
+    <button @click="updateOneTaskByTaskId">updateOneTaskByTaskId</button>
   </div>
 </template>
 
 
 <script>
-import { mydb } from '../../utils/mydb.js'
-import { api } from '../../utils/api.js'
+import { api } from "../../utils/api.js";
 export default {
   data() {
     return {
-      txt: '社区'
-    }
+      txt: "社区"
+    };
   },
 
   created() {
     // let app = getApp()
-
   },
   methods: {
-    // insert(){
-    //   let user = {
-    //     'name':'z1h',
-    //     'billing':1000
-    //   }
-    //   api.insertOneUser(user).then((res)=>{
-    //     console.log(res)
-    //   }).catch((rej)=>{
-    //     console.warn(rej)
-    //   })
-    // },
-    // query(){
-    //  api.queryUsersByUserName('zhang').then((res)=>{
-    //     console.log(res)
-    //   }).catch((rej)=>{
-    //     console.warn(rej)
-    //   })
-    // }
-  },
-}
+    insertOneUser() {
+      let user = {
+        name: "庄蚊子", //String real name
+        studentId: "16340222", //String studient id
+        gender: "male", //String
+        tasks: {
+          //Object the tasks which containd the user
+          published: [], // String Array the tasks that the user published
+          finished: [], //String Array the tasks that the user finished
+          doing: [] //String Array the tasks that the user is doing right now
+        },
+        idlePay: 1000 //Number free money coin
+      };
+      api
+        .insertOneUser(user)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(rej => {
+          console.warn(rej);
+        });
+    },
+    queryOneUserByUserId() {
+      api
+        .queryOneUserByUserId("ee3099285cbc14b20396f579202a0615")
+        .then(res => {
+          console.log(res);
+        })
+        .catch(rej => {
+          console.warn(rej);
+        });
+    },
+    updateOneUserById() {
+      api
+        .updateUserByUserId("ee3099285cbc14b20396f579202a0615", {
+          idlePay: 250
+        })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(rej => {
+          console.warn(rej);
+        });
+    },
+    insertOneTask() {
+      let task = {
+        name: "唱歌", // String every task need a name Title
+        type: "娱乐", //String task type
+        description: "唱一首单身情歌", //String task detail description
+        state: "publishing", //String task state
+        numberOfJoiner: 10, //Number Max Nubmer of joiners
+        joiner: [], //String Array the joiners _id array
+        location: "广州",
+        publish: {
+          publisher: "ee3099285cbc14b20396f579202a0615", //String user._id
+          beginTime: "",
+          endTime: ""
+        },
+        idlePay: 200,
+        work: {
+          beginTime: "",
+          endTime: ""
+        }
+      };
+      api
+        .insertOneTask(task)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(rej => {
+          console.warn(rej);
+        });
+    },
+    queryOneTaskByTaskId() {
+      api
+        .queryOneTaskByTaskId("96c1cbbe5cbc158b039788674988fd4a")
+        .then(res => {
+          console.log(res);
+        })
+        .catch(rej => {
+          console.warn(rej);
+        });
+    },
+    updateOneTaskByTaskId() {
+      api
+        .updateTaskByTaskId("96c1cbbe5cbc158b039788674988fd4a", {
+          idlePay: 2500
+        })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(rej => {
+          console.warn(rej);
+        });
+    }
+  }
+};
 </script>
 
 <style scoped>
-.ttt {
-  size: 50px;
-}
 </style>
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,39 +2,242 @@
 /* eslint-disable space-before-function-paren */
 import { mydb } from './mydb.js'
 export var api = {
-  queryUsersByUserName: function (username) {
-    return new Promise((res, rej) => {
-      mydb.queryOneUserByUserName(username, function (err, result) {
-        if (result.data.length != 0) {
-          res(result)
-        } else {
-          rej('no query')
-        }
-      })
 
-    })
-  },
+  /**
+   * insert a user in server
+   * @param {Object} user user object
+   */
   insertOneUser: function (user) {
-    return new Promise((res, rej) => {
-      mydb.queryOneUserByUserName(user.name, function (err, result) {
+    return new Promise((resolve, reject) => {
+      if (!isVaildUserForm(user)) {
+        let msg = {
+          'result': 'userform is invalid ',
+          'errMsg': 'insert a user:error'
+        }
+        reject(msg)
+      }
+      mydb.queryOneUserByUserName(user.name, function (result) {
         if (result.data.length != 0) {
-          rej(result + 'already has')
+          let msg = {
+            'result': 'already has',
+            'errMsg': 'insert a user:error'
+          }
+          reject(msg)
         } else {
-          mydb.insertOneUser(user, function (err, result) {
-            if (result && !err) {
-              res('insert ok' + result)
+          mydb.insertOneUser(user, function (result) {
+            if (result._id) {
+              let msg = {
+                'result': result._id,
+                'errMsg': 'insert a user:ok'
+              }
+              resolve(msg)
             } else {
-              rej('insert error')
+              let msg = {
+                'result': null,
+                'errMsg': 'insert a user:error'
+              }
+              reject(msg)
             }
           })
         }
       })
 
     })
+  },
+  /**
+    * queryUsersByUserName
+    * @param {String} username user name
+    */
+  queryOneUserByUserName: function (username) {
+    return new Promise((resolve, reject) => {
+      mydb.queryOneUserByUserName(username, function (result) {
+        if (result.data.length != 0) {
+          let msg = {
+            'result': result.data,
+            'errMsg': 'query a user:ok'
+          }
+          resolve(msg)
+        } else {
+          let msg = {
+            'result': null,
+            'errMsg': 'query a user:error'
+          }
+          reject(msg)
+        }
+      })
+
+    })
+  },
+  /**
+   * queryOneUserByUserId
+   * @param {String} id user id
+   */
+  queryOneUserByUserId: function (id) {
+    return new Promise((resolve, reject) => {
+      mydb.queryOneUserByUserId(id, function (result) {
+        if (result.data.length != 0) {
+          let msg = {
+            'result': result.data,
+            'errMsg': 'query a user:ok'
+          }
+          resolve(msg)
+        } else {
+          let msg = {
+            'result': null,
+            'errMsg': 'query a user:error'
+          }
+          reject(msg)
+        }
+      })
+    })
+  },
+  /**
+   * updateUserByUserId
+   * @param {String} _id user _id
+   * @param {Object} updateInfo update user info
+   */
+  updateUserByUserId: function (_id, updateInfo) {
+    return new Promise((resolve, reject) => {
+      mydb.updateOneUserByUserId(_id, updateInfo, function (result) {
+        if (result.stats.updated == 1) {
+          let msg = {
+            'result': result.stats.updated,
+            'errMsg': 'update a user:ok'
+          }
+          resolve(msg)
+        } else {
+          let msg = {
+            'result': null,
+            'errMsg': 'update a user:error'
+          }
+          reject(msg)
+        }
+      })
+    })
+  },
+  /**
+   * insert a task and update publiser
+   * @param {Object} task task
+   */
+  insertOneTask: async function (task) {
+    let user;
+    let error;
+    await new Promise((resolve, reject) => {
+      if (!isVaildTaskForm(task)) {
+        reject('task form is valid' + task);
+      }
+      mydb.insertOneTask(task, (result) => {
+        if (result._id) {
+          resolve(result);
+        }
+        else {
+          reject('error insert task');
+        }
+      });
+    }).then((result) => {
+      task._id = result._id;
+    }).catch((ref) => {
+      error = ref;
+    });
+    await new Promise((resolve, reject) => {
+      mydb.queryOneUserByUserId(task.publish.publiser, (result) => {
+        if (result.data.length != 0) {
+          resolve(result);
+        }
+        else {
+          reject('error insert task');
+        }
+      });
+    }).then((result) => {
+      user = result.data
+    }).catch((ref) => {
+      error = ref;
+    });;
+
+    return new Promise((resolve, reject) => {
+
+      if (error) {
+        reject(error)
+      }
+      let publishs = user.tasks.published;
+      publishs.push(task._id);
+      let updateInfo = {
+        'tasks': {
+          'published': publishs
+        }
+      };
+      mydb.updateOneUserByUserId(user._id, updateInfo, (result) => {
+        if (result.stats.updated == 1) {
+          let msg = {
+            'result': task._id,
+            'errMsg': 'insert a task:ok'
+          }
+          resolve(msg);
+        }
+        else {
+          let msg = {
+            'result': null,
+            'errMsg': 'insert a task:error'
+          }
+          reject(msg);
+        }
+      });
+    });
+
+  },
+
+  /**
+   * update a Task by _id
+   * @param {String} id task._id
+   * @param {Object} updateInfo Object info
+   */
+  updateTaskByTaskId: function (id, updateInfo) {
+    return new Promise((resolve, reject) => {
+      mydb.updateOneTaskByTaskId(id, updateInfo, (result) => {
+        if (result.stats.updated == 1) {
+          let msg = {
+            'result': result.stats.updated,
+            'errMsg': 'update a task:ok'
+          }
+          resolve(msg);
+        } else {
+          let msg = {
+            'result': null,
+            'errMsg': 'update a task:error'
+          }
+          reject(msg);
+        }
+      })
+    })
+
+  },
+  queryOneTaskByTaskId: function (id) {
+    return new Promise((resolve, reject) => {
+      mydb.queryOneTaskByTaskId(id, (result) => {
+        if (result.data.length != 0) {
+          let msg = {
+            'result': result.data,
+            'errMsg': 'query a task:ok'
+          }
+          resolve(msg);
+        } else {
+          let msg = {
+            'result': null,
+            'errMsg': 'query a task:error'
+          }
+          reject(msg);
+        }
+      })
+    })
   }
+
 
 }
 
-function vaildUserForm(user) {
+function isVaildUserForm(user) {
+  return true
+}
+
+function isVaildTaskForm(user) {
   return true
 }
