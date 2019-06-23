cf58d479b3f59cd0e08f04271798fe767fc365d3
zhangziquan <ziquanzhang@126.com>
2019-06-23 15:21:29 +0800

    fix the bug of missionlist

--- static/functions/querySome/index.js
+++ static/functions/querySome/index.js
@@ -6,6 +6,7 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   try {
+    console.log(event)
     return await db.collection(event.colName).where(event.queryInfo).orderBy('createTime', 'desc').get()
   } catch (e) {
     console.log(e)
